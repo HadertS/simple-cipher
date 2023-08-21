b88e17217a86a2ed5ce4c62107930b36a4a1d2f8
HadertS <source.zero@googlemail.com>
2023-08-21 17:51:27 +0100

    Seperated some app.js functionality to server.js for scalability

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const app = express()
-const router = express.Router()
-const port = 3000
 
 const logMiddleware = require('./src/middleware/log')
 
@@ -11,6 +9,4 @@ app.use(logMiddleware)
 
 app.use('/caesar',caesarCipherRoute)
 
-app.listen(port, () => {
-  console.log(`Simple Cipher listening on port ${port}`)
-})
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,7 @@
+const app = require('./app');
+
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`Simple Cipher listening on port ${port}`)
+  })
