922265b6ecdb6d09c6f0b443d545b543c561d18f
HadertS <source.zero@googlemail.com>
2023-08-21 16:01:29 +0100

    route for ceaser cipher added

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+const router = express.Router()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const ceaserCipher = require ('./src/routes/ceaserCipher')
+
+app.use('/ceaser',ceaserCipher)
 
 app.listen(port, () => {
   console.log(`Simple Cipher listening on port ${port}`)
