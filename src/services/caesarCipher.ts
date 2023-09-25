6a91d3aaf5cc3874ecd033c7862574c6052f245a
HadertS <source.zero@googlemail.com>
2023-09-25 15:20:40 +0100

    further TDD tests and resultant bug fixes

--- src/services/caesarCipher.ts
+++ src/services/caesarCipher.ts
@@ -1,14 +1,13 @@
 export function encipher (plaintext:string,direction:string,shift:number){
     let ciphertext:string = ""
     let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
     for (let i = 0; i < shift; i++) { 
-        if (direction = "+") {
-            let tempCharacter: unknown = alphabet.shift
-            alphabet.push(tempCharacter as string)
-        } else if (direction = "-") {
-            let tempCharacter: unknown = alphabet.pop
-            alphabet.unshift(tempCharacter as string)
+        if (direction === "+") {
+            let tempCharacter: string = alphabet.shift()!
+            alphabet.push(tempCharacter)
+        } else if (direction === "-") {
+            let tempCharacter: string = alphabet.pop()!
+            alphabet.unshift(tempCharacter)
         }
     }
 
@@ -16,9 +15,8 @@ export function encipher (plaintext:string,direction:string,shift:number){
 
     for (let i = 0; i < plaintext.length; i++){
         if ((plaintext.charCodeAt(i)>96)&&(plaintext.charCodeAt(i)<123)){
-            ciphertext += alphabet[plaintext.charCodeAt(i) - 96]
-        }
-        else {
+            ciphertext += alphabet[plaintext.charCodeAt(i) - 97]
+        } else {
             ciphertext += plaintext[i]
         }
     }
--- test/unit/services/caesarCipher.test.ts
+++ test/unit/services/caesarCipher.test.ts
@@ -8,13 +8,34 @@ describe('Caesar Cipher Enciphering', () => {
         let shift:number = 1
         expect(encipher(input,direction,shift)).toBe("UFTU UFYU")
     })
-
+    it('Forward shift 12 should return \'FQEF FQJF\'', async () => {
+      let input:string = "Test Text"
+      let direction:string = "+"
+      let shift:number = 12
+      expect(encipher(input,direction,shift)).toBe("FQEF FQJF")
   })
-  describe('',()=>{
-    it('Forward shift 13 should return \'GRFG GRKG\'', async () => {
+  it('Forward shift 13 should return \'GRFG GRKG\'', async () => {
         let input:string = "Test Text"
         let direction:string = "+"
         let shift:number = 13
         expect(encipher(input,direction,shift)).toBe("GRFG GRKG")
     })
+  it('Reverse shift 1 should return \'SDRS SDWS\'', async () => {
+      let input:string = "Test Text"
+      let direction:string = "-"
+      let shift:number = 1
+      expect(encipher(input,direction,shift)).toBe("SDRS SDWS")
+  })
+  it('Reverse shift 12 should return \'HSGH HSLH\'', async () => {
+    let input:string = "Test Text"
+    let direction:string = "-"
+    let shift:number = 12
+    expect(encipher(input,direction,shift)).toBe("HSGH HSLH")
+  })
+  it('Reverse shift 13 should return \'GRFG GRKG\'', async () => {
+      let input:string = "Test Text"
+      let direction:string = "-"
+      let shift:number = 13
+      expect(encipher(input,direction,shift)).toBe("GRFG GRKG")
+  })
   })
