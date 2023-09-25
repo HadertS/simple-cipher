5b80dc7f18709e15920488c28649d71f393f9db8
HadertS <source.zero@googlemail.com>
2023-09-25 20:37:22 +0100

    eslinter added and updated based on recomendations

--- src/services/caesarCipher.ts
+++ src/services/caesarCipher.ts
@@ -1,12 +1,12 @@
 export function encipher (plaintext:string,shift:number,direction:string="+",maintainCase:boolean=false,ignoreForeignChars:boolean=false){
     let ciphertext:string = ""
-    let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     for (let i = 0; i < shift; i++) { 
         if (direction === "+") {
-            let tempCharacter: string = alphabet.shift()!
+            const tempCharacter: string = alphabet.shift()!
             alphabet.push(tempCharacter)
         } else if (direction === "-") {
-            let tempCharacter: string = alphabet.pop()!
+            const tempCharacter: string = alphabet.pop()!
             alphabet.unshift(tempCharacter)
         }
     }
--- test/unit/routes/caesarCipher.test.ts
+++ test/unit/routes/caesarCipher.test.ts
@@ -1,7 +1,7 @@
 import request from "supertest"
 import app from "../../../app"
 
-describe('GET /caesar/', () => {
+describe('get /caesar/', () => {
     it('should return \'https://en.wikipedia.org/wiki/Caesar_cipher\'', async () => {
       const res = await request(app).get('/caesar/')
       expect(res.statusCode).toBe(200)
