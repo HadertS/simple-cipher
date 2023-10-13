84188063d0def14669e1f625c9f48b9f669ad873
HadertS <source.zero@googlemail.com>
2023-10-13 14:46:22 +0100

    basic api endpoint functionality added, needs to be refined to handle optional params

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -3,6 +3,7 @@ const router = express.Router()
 
 router.use(function (err:Error, req:Request, res:Response, next:NextFunction) {
     console.error(err.stack)
+    //TODO respond with json
   })
 
   export { router };
--- src/routes/caesarCipher.ts
+++ src/routes/caesarCipher.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express"
-import { check, validationResult } from 'express-validator';
+import { body, validationResult } from 'express-validator';
+import {encipher} from '../services/caesarCipher'
 const router = express.Router()
 
 /**
@@ -9,7 +10,7 @@ const router = express.Router()
  * @apiVersion 0.0.1
  * 
  * @apiBody {string} plaintext The message to be encrypted.
- * @apiBody {number} shift The ammount the cipher alphabet should be shifted (ie given forward shift 3 A will become D)
+ * @apiBody {integer{1-25}} shift The ammount the cipher alphabet should be shifted (ie given forward shift 3 A will become D).
  * @apiBody {string=+,-} [direction=+] The direction to shift the cipher alphabet, "+" for forward  (ie A->B) or "-" for backwards (ie A->Z).
  * @apiBody {boolean=false,true} [maintainCase=false] Whether the case of the plaintext should be maintained in the ciphertext.
  * This should generally be set to false for additional security.
@@ -20,19 +21,27 @@ const router = express.Router()
  */
 
 router.post('/encode', [
-  check('plaintext').notEmpty(),
-  check('shift').notEmpty(),
-  check('direction'),
-  check('ignoreForeignChars'),
-  check('ignoreWhitespace')
+  body('plaintext').trim().notEmpty().isString(),
+  body('shift').notEmpty().isInt({min:1,max:25}),
+  body('direction').optional().trim().isString(),
+  body('ignoreForeignChars').optional().isBoolean(),
+  body('ignoreWhitespace').optional().isBoolean()
 ], (req:Request, res:Response) => {
   
   const result = validationResult(req)
+
   if (result.isEmpty()) {
-    res.send(req.body)
-  }
-  else {
-    res.send({ errors: result.array() })
+    
+    const plaintext:string = req.body.plaintext.trim()
+    const shift:number = req.body.shift
+    const direction:string = req.body.direction.trim()
+    const ignoreForeignChars:boolean = req.body.ignoreForeignChars
+    const ignoreWhitespace:boolean = req.body.ignoreWhitespace
+
+    res.send(encipher(plaintext,shift))
+
+  } else {
+    res.send({errors: result.array()})
   }
 })
 
